e6bd4dbc21128f4eb9087dbc04fe5abeef4d81ff
Davor Škobić <davor.skobic@outlook.com>
2017-05-09 19:54:47 +0200

    Vjezbe 6

---
+++ Vjezbe/Vježbe 6 - Rad sa Entity Model/Vjezbe6/Vjezbe6/Models/Pribavitelj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vjezbe6.Models
+{
+    public partial class Pribavitelj
+    {
+        public Pribavitelj()
+        {
+            Police = new HashSet<Police>();
+        }
+
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public int? Godine { get; set; }
+
+        public virtual ICollection<Police> Police { get; set; }
+    }
+}
---
+++ Vjezbe/Vježbe 6 - Rad sa Entity Model/Vjezbe6/Vjezbe6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Vjezbe6.Models;
+
+namespace Vjezbe6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //dodavanje zapisa
+            //using (var context = new InsuranceSalesContext())
+            //{
+            //    Pribavitelj a = new Pribavitelj
+            //    {
+            //        Ime = "Ivan",
+            //        Prezime = "Ivić",
+            //        Godine = 59
+            //    };
+
+            //    context.Pribavitelj.Add(a);
+            //    context.SaveChanges();
+            //}
+
+            //using (var context = new InsuranceSalesContext())
+            //{
+            //    Pribavitelj b = context.Pribavitelj.Find(3);
+            //    b.Godine = 77;
+            //    context.SaveChanges();
+            //}
+
+            using (var context = new InsuranceSalesContext())
+            {
+                Pribavitelj c = context.Pribavitelj.Find(7);
+                context.Pribavitelj.Remove(c);
+                context.SaveChanges();
+            }
+
+
+
+        }
+    }
+}
