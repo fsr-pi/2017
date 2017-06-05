5f9a3224d53b44d8030c6b3e0b8d0fad77f63000
Boris Milašinović <boris.milasinovic@fer.hr>
2017-06-05 22:15:34 +0200

    Primjer xml dokumentacije

--- Primjeri/Web/Firma.Mvc/Extensions/ExceptionExtensions.cs
+++ Primjeri/Web/Firma.Mvc/Extensions/ExceptionExtensions.cs
@@ -3,8 +3,17 @@ using System.Text;
 
 namespace Firma.Mvc.Extensions
 {
+  /// <summary>
+  /// Razred sa proširenjima za iznimke
+  /// </summary>
   public static class ExceptionExtensions
   {
+    /// <summary>
+    /// Vraća sadržaj poruka cijele hijerarhije neke iznimke. Za predanu iznimku provjerava se postoji li unutarnja iznimka.
+    /// Ako da, poruka unutanje iznimke dodaje se u rezultat te se dalje provjerava postoji li unutarnja iznimka unutarnje iznimke itd...    
+    /// </summary>
+    /// <param name="exc">Iznimka čija se kompletna hijerarhija poruka treba ispisati</param>
+    /// <returns>String formiran od poruka svih unutarnjih iznimki. Poruka svake iznimmke dodana je u novi redak</returns>
     public static string CompleteExceptionMessage(this Exception exc)
     {
       StringBuilder sb = new StringBuilder();
--- Primjeri/Web/Firma.Mvc/TagHelpers/PageLinkTagHelper.cs
+++ Primjeri/Web/Firma.Mvc/TagHelpers/PageLinkTagHelper.cs
@@ -10,10 +10,26 @@ using System.Linq;
 
 namespace Firma.Mvc.TagHelpers
 {
+  /// <summary>
+  /// Tag helper za kreiranje vlastitih poveznica na stranice u rezultatu nekog upravljača
+  /// Upotrebljava se kao atribut HTML oznake *div* 
+  /// <example>
+  /// Primjer upotrebe
+  /// <code>
+  /// <div page-info="@Model.PagingInfo" page-action="Index" page-classes-enabled="true"
+  ///      page-class="btn" page-class-normal="btn-default"
+  ///      page-class-selected="btn-primary" class="btn-group pull-right">
+  /// </div>
+  /// </code>
+  /// U datoteku *_ViewImports.cshtml* potrebno dodati `@addTagHelper *, Microsoft.AspNetCore.Mvc.TagHelpers`  
+  /// te u pogled uključiti vlastitu javascript datoteku *gotopage.js*
+  /// </example>
+  /// </summary>
   [HtmlTargetElement("div", Attributes = "page-info")]
   public class PageLinkTagHelper : TagHelper
   {
-    private readonly IUrlHelperFactory urlHelperFactory;
+
+    private readonly IUrlHelperFactory urlHelperFactory;  
     private readonly AppSettings appData;
     public PageLinkTagHelper(IUrlHelperFactory helperFactory, IOptions<AppSettings> options)
     {
@@ -25,13 +41,46 @@ namespace Firma.Mvc.TagHelpers
     [HtmlAttributeNotBound]
     public ViewContext ViewContext { get; set; }
 
+    /// <summary>
+    /// Serijalizirani string koji sadrži informacije o trenutnoj i ukupnom broju stranicu 
+    /// </summary>
     public PagingInfo PageInfo { get; set; }
+
+    /// <summary>
+    /// Serijalizirani string kojim se prenose informacije o aktivnom filtriranju podataka
+    /// </summary>
     public DokumentFilter PageFilter { get; set; }
 
+    /// <summary>
+    /// Akcija na koju poveznica treba voditi
+    /// </summary>
     public string PageAction { get; set; }
+
+    /// <summary>
+    /// Označava koriste li se stilovi za formiranje poveznica
+    /// </summary>
     public bool PageClassesEnabled { get; set; } = false;
+
+    /// <summary>
+    /// Zajednički stil za poveznice na određenu stranicu. 
+    /// Stil trenutne stranice i stil ostalih stranica se može posebno navesti
+    /// <see cref="PageClass"/>
+    /// <see cref="PageClassNormal"/>
+    /// </summary>
     public string PageClass { get; set; }
+
+    /// <summary>
+    /// Naziv CSS stila za brojeve stranica različite od trenutne
+    /// <see cref="PageClass"/>
+    /// <seealso cref="PageClassSelected"/>
+    /// </summary>
     public string PageClassNormal { get; set; }
+
+    /// <summary>
+    /// Naziv CSS stila za trenutnu stranicu
+    /// <see cref="PageClass"/>
+    /// <seealso cref="PageClassNormal"/>
+    /// </summary>
     public string PageClassSelected { get; set; }
 
     public override void Process(TagHelperContext context, TagHelperOutput output)
@@ -69,6 +118,12 @@ namespace Firma.Mvc.TagHelpers
       output.Content.AppendHtml(result.InnerHtml);     
     }
 
+
+    /// <summary>
+    /// Stvara polje za prikaz trenutne stranice i unos željene stranice
+    /// </summary>
+    /// <param name="text">Broj trenutne stranice</param>
+    /// <returns></returns>
     private TagBuilder BuildPageInputTag(string text)
     {
       IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
@@ -94,11 +149,23 @@ namespace Firma.Mvc.TagHelpers
       return tag;
     }
 
+    /// <summary>
+    /// Stvara oznaku za i-tu stranicu koristeći *i* kao sadržaj poveznice
+    /// <seealso cref="BuildTagForPage(int, string)"/>
+    /// </summary>
+    /// <param name="i">broj stranice</param>
+    /// <returns>TagBuilder s pripremljenom poveznicom</returns>
     private TagBuilder BuildTagForPage(int i)
     {
       return BuildTagForPage(i, i.ToString());
     }
 
+    /// <summary>
+    ///  Stvara oznaku za i-tu stranicu koristeći argument text kao sadržaj poveznice
+    /// </summary>
+    /// <param name="i">broj stranice</param>
+    /// <param name="text">tekst poveznice</param>
+    /// <returns>TagBuilder s pripremljenom poveznicom</returns>
     private TagBuilder BuildTagForPage(int i, string text)
     {
       IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
